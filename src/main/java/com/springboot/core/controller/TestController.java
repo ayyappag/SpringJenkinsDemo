cfde148342a91fcc084439b9693109f44acdd086
ayyappag <ayyappag@gmail.com>
2022-01-13 12:21:19 +0530

    controller impl

---
+++ src/main/java/com/springboot/core/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.springboot.core.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class TestController {
+
+	public static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+	@RequestMapping(value = "/emp", produces = "application/json")
+	public ResponseEntity<String> getName() {
+
+		logger.info("get response call");
+		return new ResponseEntity<String>("", HttpStatus.OK);
+	}
+
+}
